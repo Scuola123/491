843c9c80c695f3648787ebd2fda42dbaec048d6f
Buha <93844828+Scuola123@users.noreply.github.com>
2022-03-07 16:47:44 -0800

    Update CourseController.java

--- CourseController.java
+++ CourseController.java
@@ -9,13 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CourseController {
-    //@Autowired private CourseService courseService;
-
-    @Autowired private CourseRepository courseRepository;
-    CourseController(CourseRepository repository) {
-        this.courseRepository = repository;
-    }
-
+   @Autowired private CourseRepository courseRepository;
+    
+    
+    /*********************
+            Courses
+    *********************/
 
     @GetMapping(path="/courses")
     public String getCourses(Model model)
@@ -23,13 +22,54 @@ public class CourseController {
         Course course = new Course();
         model.addAttribute("course", courseRepository.findAll());
         model.addAttribute("id",course.getId());
-        model.addAttribute("course_name", course.getName());
-        model.addAttribute("prefix", course.getPrefix());
-        return "course/index";
-    } 
+        model.addAttribute("prefix",course.getPrefix());
+        model.addAttribute("name", course.getName());
+        model.addAttribute("units", course.getUnits());
+        model.addAttribute("type", course.getType());
+
+        return "courses";
+    } //part A
     @PostMapping("/courses")
-    public String suCourses(@ModelAttribute("course") Course course) {
+    public String submitCourses(@ModelAttribute("course") Course course) {
         System.out.println(course);
-        return "course/index";
-    } 
+        return "courses";
+    } //part B
+
+    @GetMapping("courses/edit/{id}")
+    public String showEditCourseForm(@PathVariable("id") int id, Model model) {
+        Course course = courseRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Course Id:" + id));
+        model.addAttribute("course", course);
+        return "edit_courses";
+    }
+
+    @PostMapping("courses/update/{id}")
+    public String updateStudent(@PathVariable("id") int id, Course course, BindingResult result,
+                                Model model) {
+        if (result.hasErrors()) {
+            course.setId(id);
+            return "courses";
+        }
+
+        courseRepository.save(course);
+        model.addAttribute("course", courseRepository.findAll());
+        return "courses";
+    }
+    @GetMapping("courses/new")
+    public String showNewCourseForm(Course course)
+    {
+        return "new-courses";
+    }
+
+    @PostMapping("courses/save")
+    public String addNewCourse(Course course, BindingResult result, Model model) {
+         if (result.hasErrors()) {
+          return "courses";
+          }
+        courseRepository.save(course);
+        return "redirect:/courses";
+    }
+    /****************
+        ends courses
+    ***************/
 }
